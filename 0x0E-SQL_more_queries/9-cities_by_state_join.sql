779a5a4251cb580d3e6a964ba323922a9fe22e6d
pasignature <pasignature@gmail.com>
2020-07-22 18:58:51 +0000

    updates 9

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,2 +1,4 @@
 --lists all cities where state id is the same in cities' state id
-SELECT cities.id, cities.name, states.name FROM states, cities WHERE cities.state_id = states.id ORDER BY cities.id ASC;
+SELECT cities.id, cities.name, states.name
+FROM cities
+JOIN states ON cities.state_id = states.id;
