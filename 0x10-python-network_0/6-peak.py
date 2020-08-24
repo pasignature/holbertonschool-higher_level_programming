32956dcb075a6049345554be323fce8c0757467d
pasignature <pasignature@gmail.com>
2020-08-24 22:15:29 +0000

    creates peak

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def find_peak(list_of_integers):
+    """  finds a peak in a list of unsorted integers. """
+    ln = len(list_of_integers)
+    if ln == 0:
+        return
+    m = ln // 2
+    pivot = list_of_integers[m]
+    left = list_of_integers[m - 1]
+
+    if (m == ln - 1 or pivot >= list_of_integers[m + 1]) and\
+            (m == 0 or pivot >= left):
+        return pivot
+    elif m != ln - 1 and list_of_integers[m + 1] > pivot:
+        return (find_peak(list_of_integers[m + 1:]))
+    return (find_peak(list_of_integers[:m]))
