366eb6b16069fad28ec1b961414374bf74acbf58
Andrew <pasignature@gmail.com>
2020-08-30 20:02:28 +0000

    updates

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-'''Prints state object that contains a from the db hbtn_0e_6_usa'''
+'''Prints State object with the name passed as arg'''
 
 import sys
 from model_state import Base, State
@@ -14,8 +14,11 @@ if __name__ == "__main__":
                                   sys.argv[3]),
                            pool_pre_ping=True)
     session = sessionmaker(bind=engine)()
-    result = session.query(State).all()
-    for row in result:
-        if 'a' in str(row):
-            print(row)
+    result = [row.id for row in session.query(State).
+              filter(State.name == sys.argv[4])]
+    if result:
+        for e in result:
+            print(e)
+    else:
+        print("Not found")
     session.close()
