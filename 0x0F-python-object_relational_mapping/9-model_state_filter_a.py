6fab8914db511680389e66c19e0a11aa02cde778
Andrew <pasignature@gmail.com>
2020-08-30 20:00:12 +0000

    updates

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+'''Prints the state object that contains a from the db hbtn_0e_6_usa'''
+
+if __name__ == "__main__":
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
+                           format(sys.argv[1],
+                                  sys.argv[2],
+                                  sys.argv[3]),
+                           pool_pre_ping=True)
+    session = sessionmaker(bind=engine)()
+    result = session.query(State).all()
+    for row in result:
+        if 'a' in str(row):
+            print(row)
+    session.close()
