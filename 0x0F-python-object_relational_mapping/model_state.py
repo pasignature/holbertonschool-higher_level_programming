6c691d8e76c5d075b8d63456078fb582423ccfef
Andrew <pasignature@gmail.com>
2020-08-30 19:48:47 +0000

    updates

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+'''Contains the class def of States and an instance Base = declarative_base()'''
+
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+
+Base = declarative_base()
+
+class State(Base):
+    '''Class def for State'''
+
+    __tablename__ = 'states'
+    id = Column(Integer,
+                autoincrement=True,
+                unique=True,
+                nullable=False,
+                primary_key=True)
+    name = Column(String(128),
+                  nullable=False)
+
+    def __str__(self):
+        '''str form of the class'''
+        return "{}: {}".format(self.id, self.name)
