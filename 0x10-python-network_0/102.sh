17a516fd223e545c5f5f1cacedd708478e08772a
Andrew <pasignature@gmail.com>
2020-08-31 09:31:51 +0000

    catchme if u can

--- 0x10-python-network_0/102.sh
+++ 0x10-python-network_0/102.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# makes request to 0.0.0.0:5000/catch_me and causes server to respond with message containing You got me!
+# makes request to 0.0.0.0:5000/catch_me and causes server to respond with a message containing You got me!
 curl -sL 0.0.0.0:5000/catch_me_3 -X PUT -H "Origin:HolbertonSchool"
