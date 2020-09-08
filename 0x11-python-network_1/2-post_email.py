98153183a50465cc8ee33500a954ca03d6c734cd
Andrew <pasignature@gmail.com>
2020-09-08 17:37:12 +0000

    updates

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 '''takes a url and email and sends a POST request'''
 import urllib.request as url
-import urllib.parse as urlparse
+import urllib.parse as urlp
 from sys import argv
 
 if __name__ == "__main__":
     value = {'email': argv[2]}
-    data = urlparse.urllencode(value)
+    data = urlp.urlencode(value)
     data = data.encode('ascii')
     req = url.Request(argv[1], data)
     with url.urllopen(req) as response:
