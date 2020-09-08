25bac37772e1147d6e360dcd5dae39cda745c531
Andrew <pasignature@gmail.com>
2020-09-08 21:51:02 +0000

    updates

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# Python script that fetches id from api.github.com/users
+from sys import argv
+import requests
+from requests.auth import HTTPBasicAuth
+
+if __name__ == "__main__":
+    auth = HTTPBasicAuth(argv[1], argv[2])
+    res = requests.get("https://api.github.com/user", auth=auth)
+    print(res.json().get("id"))
