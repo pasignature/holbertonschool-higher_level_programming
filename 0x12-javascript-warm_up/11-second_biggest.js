b8bc855f6a0959a5e2d989eb0a79e501c1f9382e
Andrew <pasignature@gmail.com>
2020-09-16 12:10:20 +0000

    creates second biggest

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+// finds the second largest number
+'use strict';
+(function secondBiggest (arg) {
+  let list = [];
+  if (process.argv.length < 4) {
+    console.log(0);
+  } else {
+    for (let count = 2; count < process.argv.length; count++) {
+      list.push(Number(process.argv[count]));
+    }
+    console.log(list.sort()[list.length - 2]);
+  }
+})();
