a5bc38f7cf4739fd5828793150c4011c680f1382
Andrew <pasignature@gmail.com>
2020-09-16 12:33:51 +0000

    updates 7

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
 // print C is fun by number of passed in argument
 'use strict';
-let arg = process.argv[2];
+const args = process.argv[2];
 let count = 0;
-if (isNaN(arg)) {
+if (isNaN(args)) {
   console.log('Missing number of occurrences');
 }
-while (count < arg) {
+while (count < args) {
   console.log('C is fun');
   count++;
 }
