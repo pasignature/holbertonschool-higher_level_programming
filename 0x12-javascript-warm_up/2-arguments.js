8aca8e716f06743e81afd89616183efe6b25a439
Andrew <pasignature@gmail.com>
2020-09-16 12:19:40 +0000

    updates 2

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 // prints a message depending on number of arguments
 'use strict';
-let len = process.argv.length;
+const len = process.argv.length;
 if (len < 3) {
   console.log('No argument');
 } else if (len > 3) {
