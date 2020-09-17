d7ab7c5f954c5dd7a79ae6bbd34689182b2d0e3b
Andrew <pasignature@gmail.com>
2020-09-17 11:33:20 +0000

    updates

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 //finds the second largest number
 'use strict';
-const arr = process.argv.slice(2);
+let secondMax = 0;
+
+const arr = process.argv.slice(2); // assign arr with the second arg
 if (arr.length > 1) {
   arr.sort();
-  
-// assign secondMax with the second biggest number in the sorted array
-let secondMax = arr[arr.length - 2];
+  secondMax = arr[arr.length - 2]; // assign secondMax with the second biggest number in the sorted array
 }
 console.log(secondMax);
