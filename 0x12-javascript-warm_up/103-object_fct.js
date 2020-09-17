3032e3610c7069bb5060dd9a1cd48e0a25f17aab
Andrew <pasignature@gmail.com>
2020-09-17 14:05:58 +0000

    creates increase

---
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+// increments the integer value
+let myObject = {
+  type: 'object',
+  value: 12
+};
+console.log(myObject);
+myObject.incr = function () {
+  this.value++;
+};
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
