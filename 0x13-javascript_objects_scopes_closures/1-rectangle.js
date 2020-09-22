1bb592092e5ae6b871e42954c32f0b23ff478b92
Andrew <pasignature@gmail.com>
2020-09-22 16:28:56 +0000

    creates 1

---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+// define a class and initialize with w, h
+class Rectangle {
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
+  }
+}
+module.exports = Rectangle;
