7f006ef8a997884037f3fd94fae7f5d9da4e72fb
Andrew <pasignature@gmail.com>
2020-09-22 16:38:23 +0000

    creates 4

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,30 @@
+#!/usr/bin/node
+// class Rectangle that defines a rectangle
+class Rectangle {
+  constructor (w, h) {
+    if (w <= 0 || h <= 0 || w === undefined || h === undefined) {
+      return this;
+    } else {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let count = 0; count < this.height; count++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+
+  rotate () {
+    const holder = this.width;
+    this.width = this.height;
+    this.height = holder;
+  }
+
+  double () {
+    this.width = 2 * this.width;
+    this.height = 2 * this.height;
+  }
+}
+module.exports = Rectangle;
