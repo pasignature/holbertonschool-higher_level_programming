0702f91b26e2f3b321c1ae37c741fb5ee1aadd1f
Andrew <pasignature@gmail.com>
2020-09-22 16:31:11 +0000

    creates 2

---
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+// defines a class, initialize values if positive integer
+class Rectangle {
+  constructor (w, h) {
+    if (w <= 0 || h <= 0 || w === undefined || h === undefined) {
+      return this;
+    } else {
+      this.width = w;
+      this.height = h;
+    }
+  }
+}
+module.exports = Rectangle;
