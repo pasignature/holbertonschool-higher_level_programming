7cd165e9ac0c9cfd4a0987ec522301dc5390eef8
Andrew <pasignature@gmail.com>
2020-09-22 16:50:11 +0000

    creates 4, 5

---
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -0,0 +1,36 @@
+#!/usr/bin/node
+// creates a class that extends another class
+class Rectangle {
+  constructor (w, h) {
+    if (w <= 0 || h <= 0 || w === undefined || h === undefined) {
+      return this;
+    } else {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let count = 0; count < this.height; count++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+
+  rotate () {
+    let holder = this.width;
+    this.width = this.height;
+    this.height = holder;
+  }
+
+  double () {
+    this.width = 2 * this.width;
+    this.height = 2 * this.height;
+  }
+}
+
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+}
+module.exports = Square;
