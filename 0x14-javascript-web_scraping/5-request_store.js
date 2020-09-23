1fd5d014c6905291c4a59e5fb314a349e8cf057e
Andrew <pasignature@gmail.com>
2020-09-23 21:30:08 +0000

    creates request store 5

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+'use strict'
+
+const requestMod = require('request');
+const requestUrl = process.argv[2];
+const fs = require('fs');
+requestMod(requestUrl, 'utf-8').pipe(fs.createWriteStream(process.argv[3]));
