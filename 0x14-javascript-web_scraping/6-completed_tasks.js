b3f8c893fe20b3f16b1f26bf415e934f737732ae
Andrew <pasignature@gmail.com>
2020-09-23 21:31:19 +0000

    creates complete tast

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,25 @@
+#!/usr/bin/node
+
+'use strict'
+
+const requestMod = require('request');
+const requestUrl = process.argv[2];
+
+requestMod(requestUrl, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    const result = JSON.parse(body);
+    const dic = {};
+    for (let index = 0; index < result.length; index++) {
+      if (result[index].completed === true) {
+        if (dic[result[index].userId] === undefined) {
+          dic[result[index].userId] = 1;
+        } else {
+          dic[result[index].userId] += 1;
+        }
+      }
+    }
+    console.log(dic);
+  }
+});
