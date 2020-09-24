65e16644345e8fdf67e295d2ffeabd9e2a1ee989
Andrew <pasignature@gmail.com>
2020-09-24 21:44:26 +0000

    creates 102

---
+++ 0x15-javascript-web_jquery/102-script.js
@@ -0,0 +1,13 @@
+// fetches and prints how to say 'Hello'
+
+'use strict';
+
+const $ = window.$;
+window.onload = function () {
+  $('INPUT#btn_translate').click(function () {
+    const lan = $('INPUT#language_code').val();
+    $.get('https://fourtonfish.com/hellosalut/?lang=' + lan, function (data, textStatus) {
+      $('DIV#hello').text(data.hello);
+    });
+  });
+};
